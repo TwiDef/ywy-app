60613846f10cf8f2173b2acf7520d77441165f59
Def <deferenseo@gmail.com>
2024-11-06 19:15:07 +0300

    added/fix styles to search(input)

--- src/components/pages/Search/Search.jsx
+++ src/components/pages/Search/Search.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Stack, TextField } from '@mui/material';
+import { Stack, TextField, Button, Box } from '@mui/material';
 import { useGetFilmByKeywordQuery } from '../../../services/kinopoiskApi';
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 import { theme } from '../../../theme';
 
 import styles from './Search.module.css';
@@ -17,28 +18,46 @@ const Search = () => {
   /* console.log(inputValue) */
 
   return (
-    <Stack sx={{ display: "flex", alignItems: "center" }}>
-      <TextField
-        className={styles.textField}
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        label="Фильмы, сериалы, мульфильмы"
+    <Stack sx={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+      <Stack
+        className={styles.searchBlock}
         sx={{
           mt: 4, width: { xs: "100%", md: "60%" },
-          '& label': { color: theme.pale_purple },
-          '& label.Mui-focused': { color: theme.white },
-          '& .MuiOutlinedInput-root': {
-            '& fieldset': { borderColor: theme.pale_purple, borderRadius: "14px" },
-            '&:hover fieldset': { borderColor: theme.white },
-            '&.Mui-focused fieldset': { borderColor: theme.white }
-          }
-        }}
-        slotProps={{
-          input: {
-            startAdornment: inputValue && <SearchIcon sx={{ color: theme.white }} />
-          }
-        }}
-      />
+        }}>
+        <TextField
+          className={styles.textField}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          label="Фильмы, сериалы, мульфильмы"
+          sx={{
+            width: "100%",
+            '& label': { color: theme.pale_purple },
+            '& label.Mui-focused': { color: theme.white },
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': { borderColor: theme.pale_purple, borderRadius: "14px" },
+              '&:hover fieldset': { borderColor: theme.white },
+              '&.Mui-focused fieldset': { borderColor: theme.white }
+            }
+          }}
+          slotProps={{
+            input: {
+              startAdornment: inputValue && <SearchIcon sx={{ color: theme.white }} />
+            }
+          }}
+        />
+        <Button
+          className={`${styles.clearBtn} ${!inputValue && styles.hidden}`}
+          onClick={() => setInputValue("")}
+          sx={{
+            position: "absolute",
+            top: 10,
+            right: 0,
+            "&:hover, &:focus": { background: "transparent", scale: 1.1 },
+            "&:active": { background: "transparent" }
+          }}>
+          <CloseIcon sx={{ color: theme.white }} />
+        </Button>
+      </Stack>
     </Stack>
   );
 };
